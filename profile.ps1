a9af214b60588713ca87514aa706c5764d1ee5af
Krzysztof Tomczak <krzysztof.tomczak@outlook.com>
2020-04-18 10:44:57 +0200

    Add check for posh-git installation

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,9 @@
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineOption -BellStyle None
-Import-Module posh-git
+
+if (Get-Module -ListAvailable -Name posh-git) {
+    Import-Module posh-git
+}
+else {
+    Write-Output "posh-git not installed"
+}
